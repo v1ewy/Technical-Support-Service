a7b3c4a3070569674e7722c62cbf2d69b44fd03f
Vedenov Ivan <fo4ikvanka@gmail.com>
2026-03-15 14:41:04 +0300

    Add structures

--- Technical Support Service(2.DS)/Technical Support Service(2.DS)/sup_serv.c
+++ Technical Support Service(2.DS)/Technical Support Service(2.DS)/sup_serv.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+typedef struct Request {
+    int id;
+    char username[256];
+    char priority;
+    char timestamp[10];
+} Request;
+
+typedef struct QueueNode {
+    Request data;
+    struct QueueNode* next;
+} QueueNode;
+
+typedef struct Queue {
+    QueueNode* front;
+    QueueNode* rear;
+    int size;
+} Queue;
+
+typedef struct HistoryReq {
+    char department;
+    Request data;
+    struct HistoryReq* next;
+} HistoryReq;
+
 int main(void) {
     printf("Hello, World!\n");
     return 0;
