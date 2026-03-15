a084eb9726a10bfe81c0ef0c86ca7bbd8914d14f
Vedenov Ivan <fo4ikvanka@gmail.com>
2026-03-15 15:15:31 +0300

    Add add_req and create_req functions

--- Technical Support Service(2.DS)/Technical Support Service(2.DS)/sup_serv.c
+++ Technical Support Service(2.DS)/Technical Support Service(2.DS)/sup_serv.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int identifier = 0;
 
 typedef struct Request {
     int id;
     char username[256];
-    char priority;
-    char timestamp[10];
+    int priority;
+    time_t timestamp;
 } Request;
 
 typedef struct QueueNode {
@@ -18,13 +23,95 @@ typedef struct Queue {
     int size;
 } Queue;
 
-typedef struct HistoryReq {
+typedef struct History {
     char department;
     Request data;
     struct HistoryReq* next;
-} HistoryReq;
+} History;
+
+void init_queue (struct Queue* queue) {
+    queue->front = NULL;
+    queue->rear = NULL;
+    queue->size = 0;
+}
+
+void create_req(Request* request) {
+    request->id = identifier++;
+    
+    printf("Введите имя\n");
+    printf("> ");
+    getchar();
+    
+    fgets(request->username, 256, stdin);
+    request->username[strcspn(request->username, "\n")] = '\0';
+    
+    printf("Введите приоритет (1-5)\n");
+    printf("> ");
+    
+    scanf("%d", &request->priority);
+    
+    request->timestamp = time(NULL);
+    
+}
+
+void add_request(Queue* department) {
+    Request request;
+    create_req(&request);
+    
+    struct QueueNode* new_node = (struct QueueNode*)malloc(sizeof(QueueNode));
+    new_node->data = request;
+    
+    if (department->front == NULL || request.priority < department->front->data.priority) {
+        new_node->next = department->front;
+        department->front = new_node;
+        
+        if (department->rear == NULL) {
+            department->rear = new_node;
+        }
+    } else {
+        struct QueueNode* current = department->front;
+        while (current->next != NULL && request.priority >= current->next->data.priority) {
+            current = current->next;
+        }
+        new_node->next = current->next;
+        current->next = new_node;
+        if (new_node->next == NULL) {
+            department->rear = new_node;
+        }
+    }
+}
+
+
 
 int main(void) {
-    printf("Hello, World!\n");
+    History* history = NULL;
+    Queue first;
+    Queue second;
+    Queue third;
+    init_queue(&first);
+    init_queue(&second);
+    init_queue(&third);
+    
+    int department;
+    printf("Номер подразделения(1-3)\n");
+    printf("> ");
+    scanf("%d", &department);
+    
+    switch (department) {
+        case 1:
+            add_request(&first);
+            break;
+        case 2:
+            add_request(&second);
+            break;
+        case 3:
+            add_request(&third);
+            break;
+            
+        default:
+            printf("Введен не правильный номер подразделения");
+            break;
+    }
+    
     return 0;
 }
